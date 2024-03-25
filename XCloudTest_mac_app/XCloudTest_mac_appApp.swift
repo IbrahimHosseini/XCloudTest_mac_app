6406394b5b8b2b0224d323c05e436056a7f58023
Ibrahim Hosseini <sehosseini@me.com>
2024-03-25 16:34:58 +0330

    Initial Commit

---
+++ XCloudTest_mac_app/XCloudTest_mac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCloudTest_mac_appApp.swift
+//  XCloudTest_mac_app
+//
+//  Created by Ibrahim on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct XCloudTest_mac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCloudTest_mac_appUITests/XCloudTest_mac_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XCloudTest_mac_appUITestsLaunchTests.swift
+//  XCloudTest_mac_appUITests
+//
+//  Created by Ibrahim on 3/25/24.
+//
+
+import XCTest
+
+final class XCloudTest_mac_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
